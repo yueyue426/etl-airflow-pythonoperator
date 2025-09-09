ef583ac4e89824e4895ca3de46172aec5cbd68fa
Chloe Jiang <yueyue426@berkeley.edu>
2025-09-09 11:23:00 -0700

    Define ETL functions

---
+++ ETL_toll_data.py
@@ -0,0 +1,109 @@
+# Import libraries
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+import requests
+import tarfile
+import csv
+import shutil
+import pandas as pd
+
+# Define the path for the input and output file
+source_url = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0250EN-SkillsNetwork/labs/Final%20Assignment/tolldata.tgz'
+destination_path = '$PWD/airflow/dags/python_etl/staging'
+
+# Function to download the dataset
+def download_dataset():
+    response = requests.get(source_url) 
+    if response.status_code == 200: # 200 means the request succeed
+        with open(f"{destination_path}/tolldata.tgz", 'wb') as f: 
+            f.write(response.content)
+    else:
+        print("Failed to download the file.")
+
+# Function to untar the file
+def untar_dataset():
+    with tarfile.open(f"{destination_path}/tolldata.tgz", "r:gz") as tar:
+        tar.extractall(path=destination_path)
+
+# Function to extract data from CSV
+def extract_data_from_csv():
+    input_file = f"{destination_path}/vehicle-data.csv"
+    output_file = f"{destination_path}/csv_data.csv"
+    with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(['Rowid', 'Timestamp', 'Anonymized Vehicle number', 'Vehicle type'])
+        for line in infile:
+            row = line.strip().split(',')
+            writer.writerow([row[0], row[1], row[2], row[3]])
+
+# Function to extract data from TSV
+def extract_data_from_tsv():
+    input_file = f"{destination_path}/tollplaza-data.tsv"
+    output_file = f"{destination_path}/tsv_data.csv"
+    with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(['Number of axles', 'Tollplaza id', 'Tollplaza code'])
+        for line in infile:
+            row = line.split('\t')
+            writer.writerow([row[0], row[1], row[2]])
+
+# Function to extract data from fixed width file
+def extract_data_from_fixed_width():
+    input_file = f"{destination_path}/payment-data.txt"
+    output_file = f"{destination_path}/fixed_width_data.csv"
+    with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(['Type of Payment code', 'Vehicle Code'])
+        for line in infile:
+            writer.writerow([line[50:53].strip(), line[54:].strip()])
+
+# Function to consolidate data
+def consolidate_data():
+    csv_file = f"{destination_path}/csv_data.csv"
+    tsv_file = f"{destination_path}/tsv_data.csv"
+    fixed_width_file = f"{destination_path}/fixed_width_data.csv"
+    output_file = f"{destination_path}/extracted_data.csv"
+
+    # with open(csv_file, 'r') as csv_in, \
+    #      open(tsv_file, 'r') as tsv_in, \
+    #      open(fixed_width_file, 'r') as fixed_in, \
+    #      open(output_file, 'w') as out_file:
+    #     csv_reader = csv.reader(csv_in)
+    #     tsv_reader = csv.reader(tsv_in)
+    #     fixed_reader = csv.reader(fixed_in)
+    #     writer = csv.writer(out_file)
+
+    #     writer.writerow(['Rowid', 'Timestamp', 'Anonymized Vehicle number', 'Vehicle type', 'Number of axles', 'Tollplaza id', 'Type of Payment code', 'Vehicle Code'])
+    #     next(csv_reader)
+    #     next(tsv_reader)
+    #     next(fixed_reader)
+
+    #     for csv_row, tsv_row, fixed_row in zip(csv_reader, tsv_reader, fixed_reader):
+    #         writer.writerow(csv_row + tsv_row + fixed_row)
+
+    df_csv = pd.read_csv(csv_file)
+    df_tsv = pd.read_csv(tsv_file, sep='\t')
+    df_fixed = pd.read_csv(fixed_width_file)
+    df_all = pd.concat([df_csv, df_tsv, df_fixed], axis=1)
+    df_all.to_csv(output_file, index=False)
+
+# Function to transform data
+def transform_data():
+    input_file = f"{destination_path}/extracted_data.csv"
+    output_file = f"{destination_path}/transformed_data.csv"
+
+    # with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+    #     reader = csv.DictReader(infile)
+    #     writer = csv.DictWriter(outfile, fieldnames=reader.fieldnames)
+    #     writer.writeheader()
+
+    #     for row in reader:
+    #         row['Vehicle type'] = row['Vehicle type'].upper()
+    #         writer.writerow(row)
+
+    df = pd.read_csv(input_file)
+    df["Vehicle type"] = df["Vehicle type"].str.upper()
+
+    df.to_csv(output_file, index=False)
